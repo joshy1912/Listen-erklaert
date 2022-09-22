d8fac98f679f492d9a495a7d519701e790db3ce1
joshy1912 <99416967+joshy1912@users.noreply.github.com>
2022-09-22 17:18:06 +0200

    Add files via upload

---
+++ listen.py
@@ -0,0 +1,13 @@
+#Um zu bestimmen, wie viele Elemente eine Liste hat, verwenden Sie die len()Funktion:
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+
+
+#Sagt dir was dein liste für ein datentyp ist
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+#Es ist auch möglich, den list()- Konstruktor beim Erstellen einer neuen Liste zu verwenden.
+hislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
